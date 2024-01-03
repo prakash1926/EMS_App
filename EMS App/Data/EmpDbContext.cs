3b1b4eab5905925e24d0470d4b9d8dd132d35282
prakash1926 <ananthaprakash71@gmail.com>
2024-01-03 18:12:45 +0530

    Initial commit

---
+++ EMS App/Data/EmpDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EMS_App.Models;
+
+namespace EMS_App.Data
+{
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext (DbContextOptions<EmpDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EMS_App.Models.DeptMaster> DeptMaster { get; set; } = default!;
+
+        public DbSet<EMS_App.Models.EmpProfile>? EmpProfile { get; set; }
+    }
+}
---
+++ EMS App/Models/DeptMaster.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMS_App.Models
+{
+    [Table("Department")]
+    public class DeptMaster
+    {
+        [Key]
+        public  int DeptCode { get; set; }
+        public string? DeptName { get; set;}
+
+        public virtual ICollection<EmpProfile>? EmpProfiles { get; set; }
+    }
+}
